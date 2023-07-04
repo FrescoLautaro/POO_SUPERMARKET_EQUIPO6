a9e7fba0734399ca07d40fdce0b6f17a38d1949b
Lautaro Fresco <127969688+FrescoLautaro@users.noreply.github.com>
2023-07-04 09:44:50 -0300

    Update Venta.java

--- Venta.java
+++ Venta.java
@@ -9,10 +9,66 @@
  */
 public class Venta {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
+   private LocalDate fecha;
+    private static int cantTickets;
+    private int numeroTicket;
+     private ArrayList<Producto> listProductos = new ArrayList<>();
+
+    public Venta(LocalDate fecha, int numeroTicket) {
+        this.fecha = fecha;
+        this.numeroTicket = numeroTicket;
+    }
+    
+    public double Calcular_venta(){
+        double Totalventa = 0;
+        for(Producto p: listProductos){
+            Totalventa+=p.getPrecio();
+        }
+    
+        return Totalventa;
+    }
+    
+    public double importeTotalDesc(){
+        double descontado=0;
+        
+        for(Producto p: listProductos){
+        descontado+=pn.CalcularDesc();
+        
+        }
+        return descontado;
+    }
+    
+    
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public static int getCantTickets() {
+        return cantTickets;
+    }
+
+    public static void setCantTickets(int cantTickets) {
+        Venta.cantTickets = cantTickets;
+    }
+
+    public int getNumeroTicket() {
+        return numeroTicket;
+    }
+
+    public void setNumeroTicket(int numeroTicket) {
+        this.numeroTicket = numeroTicket;
+    }
+
+    public ArrayList<Producto> getListProductos() {
+        return listProductos;
+    }
+
+    public void setListProductos(ArrayList<Producto> listProductos) {
+        this.listProductos = listProductos;
     }
 }
