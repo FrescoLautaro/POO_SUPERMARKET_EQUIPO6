d39db817e3443df3f7ddc0251add20262636cd60
Lautaro Fresco <127969688+FrescoLautaro@users.noreply.github.com>
2023-07-04 09:44:16 -0300

    Update Producto.java

--- Producto.java
+++ Producto.java
@@ -9,10 +9,51 @@
  */
 public class Producto {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
+    private String nombre;
+    private double precio;
+    private boolean precioCuidado;
+
+    public Producto(String nombre, double precio, boolean precioCuidado) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.precioCuidado = precioCuidado;
+    }
+
+    
+    public String getTipoProd(){
+        String tipo="";
+        if
+        return tipo;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Producto{" + "nombre=" + nombre + ", precio=" + precio + ", precioCuidado=" + precioCuidado + '}';
+    }
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public boolean isPrecioCuidado() {
+        return precioCuidado;
+    }
+
+    public void setPrecioCuidado(boolean precioCuidado) {
+        this.precioCuidado = precioCuidado;
     }
 }
