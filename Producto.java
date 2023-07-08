721ce43adda69f539a566e01ae0b71af8e59fcf3
Lautaro Fresco <127969688+FrescoLautaro@users.noreply.github.com>
2023-07-08 00:17:06 -0300

    Update Producto.java

--- Producto.java
+++ Producto.java
@@ -1,61 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
+package supermarket;
 
-/**
- *
- * @author ET36
- */
+
 public class Producto {
 
-    private String nombre;
-    private double precio;
-    private boolean precioCuidado;
+    protected String nombre;
+    protected double precio;
+    protected boolean esPrecioCuidado;
 
-    public Producto(String nombre, double precio, boolean precioCuidado) {
+    public Producto(String nombre, double precio, boolean esPrecioCuidado) {
         this.nombre = nombre;
         this.precio = precio;
-        this.precioCuidado = precioCuidado;
+        this.esPrecioCuidado = esPrecioCuidado;
     }
 
-    
-    public String getTipoProd(){
-        String tipo="";
-        if (precioCuidado == true) 
-            CalcularDesc();
-        else;
-        return tipo;
-    }
-    
-    
-    @Override
-    public String toString() {
-        return "Producto{" + "nombre=" + nombre + ", precio=" + precio + ", precioCuidado=" + precioCuidado + '}';
-    }
-    
-    
     public String getNombre() {
         return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
 
     public double getPrecio() {
         return precio;
     }
 
-    public void setPrecio(double precio) {
-        this.precio = precio;
+    public boolean esPrecioCuidado() {
+        return esPrecioCuidado;
     }
 
-    public boolean isPrecioCuidado() {
-        return precioCuidado;
+    public void aplicarDescuento(double descuento) {
+        this.precio -= descuento;
     }
 
-    public void setPrecioCuidado(boolean precioCuidado) {
-        this.precioCuidado = precioCuidado;
-    }
 }
