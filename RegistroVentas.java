f660e691249002ad55296a62050743ecfc3fe9d1
Lautaro Fresco <127969688+FrescoLautaro@users.noreply.github.com>
2023-07-08 00:19:10 -0300

    Update RegistroVentas.java

--- RegistroVentas.java
+++ RegistroVentas.java
@@ -1,48 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
+package supermarket;
 
-/**
- *
- * @author ET36
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class RegistroVentas {
 
-    private ArrayList<Venta> listVentas = new ArrayList<>();
+    private List<Venta> ventas;
 
     public RegistroVentas() {
+        this.ventas = new ArrayList<>();
     }
 
-    public double Calcular_total_ventas() {
-        double total = 0;
-        for (Venta v : listVentas) {
-            total += v.Calcular_venta();
+    public void agregarVenta(Venta venta) {
+        ventas.add(venta);
+    }
 
+    public double calcularTotalVentas() {
+        double total = 0;
+        for (Venta venta : ventas) {
+            total += venta.calcularTotalVenta();
         }
         return total;
     }
 
-    public double ImporteDescuento() {
-        double TotalDescontado = 0;
-        for (Venta v : listVentas) {
-            TotalDescontado += v.importeTotalDesc();
-
+    public double calcularTotalDescuentos() {
+        double totalDescuentos = 0;
+        for (Venta venta : ventas) {
+            totalDescuentos += venta.getDescuentoTotal();
         }
-
-        return TotalDescontado;
-    }
-    
-    public void Mostrar_ventas(){
-        
-    
+        return totalDescuentos;
     }
 
-    public ArrayList<Venta> getListVentas() {
-        return listVentas;
-    }
-
-    public void setListVentas(ArrayList<Venta> listVentas) {
-        this.listVentas = listVentas;
+    public void mostrarVentas() {
+        for (Venta venta : ventas) {
+            System.out.println("Venta numero: " + venta.getNumeroVenta());
+            System.out.println("Fecha: " + venta.getFecha());
+            System.out.println("Total: $" + venta.calcularTotalVenta());
+            System.out.println("Descuento aplicado: $" + venta.getDescuentoTotal());
+            System.out.println("Cantidad de productos de primera necesidad: " + venta.getCantidadProductosPrimeraNecesidad());
+            System.out.println("Cantidad de productos con precios cuidados: " + venta.getCantidadProductosPrecioCuidado());
+            System.out.println("Productos:");
+            venta.mostrarProductos();
+            System.out.println("------------------------");
+        }
+        System.out.println("Total en ventas: $" + calcularTotalVentas());
+        System.out.println("Total de descuentos: $" + calcularTotalDescuentos());
     }
 }
